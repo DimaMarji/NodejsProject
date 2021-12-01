d230ca967e4e0f526478d09aa9912859602b8851
daniaita <86915562+daniaita@users.noreply.github.com>
2021-12-01 15:35:32 +0200

    add register and login

---
+++ models/users.js
@@ -0,0 +1,53 @@
+const pool = require('./pool');
+const bcrypt = require("bcrypt");
+
+var User = (user) => {
+    this.id = user.id;
+    this.userName = user.userName;
+    this.email = user.email;
+    this.password = user.password;
+};
+
+
+User.register = (user, result) => {
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
+            result(err, null);
+        } else {
+            pool.query("INSERT INTO user SET userName = ? , email = ? , password = ?", [user.userName, user.email, hash], (err, res) => {
+                if (err) {
+                    result(err, null);
+                } else {
+                    result(null, Object.assign({ id: res.insertId }, user)); // Merge 2 objects
+                }
+            });
+        }
+    });
+};
+User.login = (userName, password, result) => {
+    pool.query('SELECT * FROM user WHERE userName = ?', userName, (err, res) => {
+        if (err) {
+            connection.release();
+            result(err, null, 500);
+        } else {
+            if (res.length === 0) { // The user is not found for the given userName
+                result({ Error: "Error in user name or password" }, null, 400);
+            } else {
+                const user = res[0];
+                bcrypt.compare(password, user.password, (err, isCorrect) => {
+                    if (err) {
+                        result(err, null, 500);
+                    } else {
+                        if (!isCorrect) {
+                            result({ Error: "Error in user name or password" }, null, 400);
+                        } else {
+                            // The user name and the password are correct
+                            result(null, user, 200);
+                        }
+                    }
+                }); // end bcrypt.compare
+            }
+        }
+    });
+};
+module.exports = User;
