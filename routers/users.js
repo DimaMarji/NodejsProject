d25a715bad9a0ff0333105a1a5aac0526756b891
DimaMarji <95295668+DimaMarji@users.noreply.github.com>
2021-12-05 13:51:49 +0200

    update user.js router

--- routers/users.js
+++ routers/users.js
@@ -110,31 +110,30 @@ router.put("/:id", (req, res) => {
     }
   });
 });
-router.get("/fav", (req, res) => { 
-    const bearerHeader = req.headers["authorization"]; 
-    if (typeof bearerHeader !== "undefined") { 
-      const bearer = bearerHeader.split(" "); 
-      const bearerToken = bearer[1]; 
-      jwt.verify(bearerToken, secretKey, (err, authData) => { 
-        if (err) { 
-          res.sendStatus(403); 
-        } else { 
-          const userId = authData.user.id; 
-          
-   
-          Book.favBook(userId, (err, status, code) => { 
-            if (err) { 
-              res.status(code).json({ error: err }); 
-            } else { 
-              res.status(200).json({ status: status }); 
-            } 
-          }); 
-        } 
-      }); 
-    } else { 
-      res.status(403).send("Invalid authorization header"); // Forbidden 
-    } 
-  });
+router.get("/fav", (req, res) => {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    jwt.verify(bearerToken, secretKey, (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const userId = authData.user.id;
+
+        Book.getFavbooks(userId, (err, books, code) => {
+          if (err) {
+            res.status(code).json({ error: err });
+          } else {
+            res.status(200).json({ books });
+          }
+        });
+      }
+    });
+  } else {
+    res.status(403).send("Invalid authorization header"); // Forbidden
+  }
+});
 router.delete("/:id", (req, res) => {
   const userId = req.params.id;
   if (isNaN(userId))
