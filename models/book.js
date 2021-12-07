823aed205548a56c47b29174de848bc4d5d4e298
AreejKarazoun <95364262+AreejKarazoun@users.noreply.github.com>
2021-12-07 04:40:50 +0200

    update and rename book model
    add to fav

--- models/book.js
+++ models/book.js
@@ -1,13 +1,21 @@
 const pool = require('./pool');
-var Book = (book) => {
-    this.id = book.id;
-    this.title = book.title;
-    this.ISBN = book.ISBN;
-    this.authorId = book.authorId;
-   
-};
+const Auths = require('./author');
+const NodeCache = require("node-cache");
+const cache = new NodeCache({ stdTTL: 120, checkperiod: 600 });
 
-Book.getAll = (result) => {
+class Book {
+    constructor(book) {
+        this.id = book.id;
+        this.title = book.title;
+        this.authorId = book.authorId;
+        this.ISBN = book.ISBN;
+        this.image = book.image;
+    }
+}
+
+
+
+Book.getAllBook = (result) => {
     pool.query('SELECT * FROM book ORDER BY id ', (err, res) => {
         if (err) {
             result(err, null);
@@ -57,21 +65,25 @@ Book.getByTitle = (booktitle, result) => {
     }
 };
 
+
+
+
 Book.createBook = (newBook, result) => {
-    pool.query("INSERT INTO book (title ,authorId,ISBN,image ) VALUES ( ?,?,?,? )", [newBook.title, newBook.Author.getAllAuthors,
+    pool.query("INSERT INTO book (title ,authorId,ISBN,image ) VALUES ( ?,?,?,? )", [newBook.title, newBook.authorId,
     newBook.ISBN, newBook.image], (err, res) => {
         if (err) {
             console.log("error", err);
             result(err, null, 500);
         } else {
             console.log(res)
-            result(null, { "id": res.insertId, "title": newBook.title, "authorId": Auths.getAllAuthors, "ISBN": newBook.ISBN, "image": newBook.image });
+            result(null, { "id": res.insertId, "title": newBook.title, "authorId": newBook.authorId, "ISBN": newBook.ISBN, "image": newBook.image });
 
 
         }
     });
 
 };
+
 
 Book.deleteBook = (bookId, result) => {
     pool.getConnection((err, connection) => {
@@ -104,6 +116,53 @@ Book.deleteBook = (bookId, result) => {
     });
 };
 
+Book.addBookToFav =(newfavbook,result)=>{
+
+    pool.query("INSERT INTO userbooksfav (userId ,bookId,favOrder ) VALUES ( ?,?,?)", [newfavbook.userId, newfavbook.bookId,newfavbook.favOrder], 
+    (err, res) => {
+            if (err) {
+                console.log("error", err);
+                result(err, null, 500);
+            } else {
+                console.log(res)
+                result(null, { "id": res.insertId, "userId": newfavbook.userId, "bookId": newfavbook.bookId, "favOrder": newfavbook.favOrder });
+    
+    
+            }
+        });
+}
+
+
+Book.deleteBookFromFav = (userId,bookId, result) => {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            result(err, null, 500);
+        } else {
+            connection.query(`SELECT * FROM userbooksfav WHERE bookId = ${bookId}`, (err, resGet) => {
+                if (err) {
+                    connection.release();
+                    return result(err, null, 500);
+                } else {
+                    if (resGet.length === 0) { // The book is not found for the givent id
+                        result({ error: 'Record not found' }, null, 404);
+                        connection.release();
+                    } else {
+                        // Use one connection to DB for the 2 queries
+                        connection.query(`DELETE FROM userbooksfav WHERE bookId = ${bookId} and userId = ${userId}`, (errDel, resDel) => {
+                            connection.release();
+                            if (errDel) {
+                                result(errDel, null, 500);
+                            } else {
+                                result(null, resGet, 200);
+                                cache.del(`favbook${bookId}`);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    });
+};
 
 Book.updateReadStatus = (userId,bookId,result) => {
     pool.query(`UPDATE userbooksfav
@@ -121,7 +180,8 @@ Book.reorder=(newFavOrder,bookId,userId,result) => {
         if (err) {
             result(err, null);
         } else {
-            connection.query(`SELECT favOrder FROM userbooksfav WHERE userId = ${userId} and bookId =  ${bookId}`, (err, res) => {
+            connection.query(`SELECT favOrder FROM userbooksfav 
+            WHERE userId = ${userId} and bookId =  ${bookId}`, (err, res) => {
         if (err) {
             connection.release();
             result(err, null, 500);
@@ -130,11 +190,13 @@ Book.reorder=(newFavOrder,bookId,userId,result) => {
             console.log(oldFav);
             console.log(newFavOrder);
             if (oldFav>newFavOrder){
-                connection.query(`UPDATE userbooksfav SET favOrder = favOrder+1 WHERE favOrder BETWEEN ${newFavOrder} and ${oldFav}-1`, (err, res) => {
+                connection.query(`UPDATE userbooksfav SET favOrder = favOrder+1 
+                WHERE favOrder BETWEEN ${newFavOrder} and ${oldFav}-1`, (err, res) => {
                     if (err) {
                         result(err, null, 500);
                     } else {
-                        connection.query(`UPDATE userbooksfav SET favOrder =  ${newFavOrder} WHERE userId = ${userId} and bookId =  ${bookId}`, (err, res) => {
+                        connection.query(`UPDATE userbooksfav SET favOrder =  ${newFavOrder} 
+                        WHERE userId = ${userId} and bookId =  ${bookId}`, (err, res) => {
                             connection.release();
                             if (err) {
                                 result(err, null, 500);
@@ -144,11 +206,13 @@ Book.reorder=(newFavOrder,bookId,userId,result) => {
                         });
                     }
                 });}else if(oldFav<newFavOrder){
-                        connection.query(`UPDATE userbooksfav SET favOrder = favOrder-1 WHERE favOrder BETWEEN ${oldFav}+1 and ${newFavOrder}`, (err, res) => {
+                        connection.query(`UPDATE userbooksfav SET favOrder = favOrder-1 
+                        WHERE favOrder BETWEEN ${oldFav}+1 and ${newFavOrder}`, (err, res) => {
                             if (err) {
                                 result(err, null, 500);
                             } else {
-                                connection.query(`UPDATE userbooksfav SET favOrder =  ${newFavOrder} WHERE userId = ${userId} and bookId =  ${bookId}`, (err, res) => {
+                                connection.query(`UPDATE userbooksfav SET favOrder =  ${newFavOrder} 
+                                WHERE userId = ${userId} and bookId =  ${bookId}`, (err, res) => {
                                     connection.release();
                                     if (err) {
                                         result(err, null, 500);
